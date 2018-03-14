ba3bcae13f9e1cce5345170dfce157ff20b1e971
Tony Medel <antonio.medel2021@email.bakersfieldcollege.edu>
2018-03-14 09:15:05 -0700

    Will convert Celsius and Fahrenheit

---
+++ project1/Conversion.java
@@ -0,0 +1,22 @@
+package project1;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public int celcius(int fahrenheit) {
+		int converted;
+		
+		return converted;
+	}
+	
+	public int fahrenheit(int celcius) {
+		int converted;
+		
+		return converted;
+	}
+
+}
